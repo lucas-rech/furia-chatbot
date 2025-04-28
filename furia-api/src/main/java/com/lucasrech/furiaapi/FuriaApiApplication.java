b2230e7404b5a82232aed6a29719bf89d9a5c62d
Lucas Rech <lucasrech00@gmail.com>
2025-04-28 13:58:38 -0300

    chore: Criado projecto backend

---
+++ furia-api/src/main/java/com/lucasrech/furiaapi/FuriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrech.furiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuriaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuriaApiApplication.class, args);
+    }
+
+}
