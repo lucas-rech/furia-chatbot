e152a2b2cc804dd72000acd3c5894396e4755a43
Lucas Rech <lucasrech00@gmail.com>
2025-04-30 11:58:48 -0300

    feat: criados testes unitários para bot service

--- furia-api/src/test/java/com/lucasrech/furiaapi/BotServiceTest.java
+++ furia-api/src/test/java/com/lucasrech/furiaapi/BotServiceTest.java
@@ -1,24 +1,27 @@
 package com.lucasrech.furiaapi;
 
+import com.lucasrech.furiaapi.exceptions.TimeOutProcessingQuestion;
 import com.lucasrech.furiaapi.services.BotService;
 import com.lucasrech.furiaapi.services.GPAPIService;
 import com.lucasrech.furiaapi.util.QuestionMatcher;
 import com.lucasrech.furiaapi.util.ReadFiles;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 
 import java.util.HashMap;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.*;
 
-@TestConfiguration
+@ExtendWith(MockitoExtension.class)
 public class BotServiceTest {
 
     @InjectMocks
@@ -37,8 +40,6 @@ public class BotServiceTest {
     void setUp() {
         gpapiService = mock(GPAPIService.class);
         botService = new BotService(gpapiService);
-        readFilesMocked = mockStatic(ReadFiles.class);
-        questionMatcherMocked = mockStatic(QuestionMatcher.class);
     }
 
     @Test
@@ -60,5 +61,34 @@ public class BotServiceTest {
         String response = botService.talkBot("qual é a cor?");
         assertEquals("A cor do PANTO é preto.", response);
     }
+
+    @Test
+    void testTalkBotWithoutMatchFallbackToGPAPI() {
+
+        readFilesMocked.when(() -> ReadFiles.readQuotesFile(anyString(), any())).thenAnswer(invocation -> null);
+        questionMatcherMocked.when(() -> QuestionMatcher.findBestMatch(any(), eq("Qual é a a fórmula para a entropia de Shannon?"))).thenReturn(null);
+
+        when(gpapiService.chatAPI("Qual é a a fórmula para a entropia de Shannon?")).thenReturn("Resposta da API");
+
+        String response = botService.talkBot("Qual é a a fórmula para a entropia de Shannon?");
+        assertEquals("Resposta da API", response);
+    }
+
+    @Test
+    void testTalkBotGPAPIException() {
+        // Mock do ReadFiles
+        readFilesMocked.when(() -> ReadFiles.readQuotesFile(anyString(), any())).thenAnswer(invocation -> null);
+
+        // Mock do QuestionMatcher
+        questionMatcherMocked.when(() -> QuestionMatcher.findBestMatch(any(), eq("qual é a cor?"))).thenReturn(null);
+
+        // Mock do GPAPIService com exceção
+        when(gpapiService.chatAPI("qual é a cor?")).thenThrow(new TimeOutProcessingQuestion());
+
+        assertThatThrownBy(() -> botService.talkBot("qual é a cor?"))
+                .isInstanceOf(TimeOutProcessingQuestion.class)
+                .hasMessageContaining("Time over processing question");
+    }
+
 
 }
