ad0bec1498d9d558d7eb7fe49d0a24724c4adde6
Lucas Rech <lucasrech00@gmail.com>
2025-04-30 10:33:09 -0300

    refactor: corrigindo commit

--- furia-api/src/main/java/com/lucasrech/furiaapi/exceptions/TimeOverProcessingQuestion.java
+++
@@ -1,8 +0,0 @@
-package com.lucasrech.furiaapi.exceptions;
-
-public class NotMatchQuestion extends RuntimeException {
-    public NotMatchQuestion() {
-
-        super("No match found for the question.");
-    }
-}
