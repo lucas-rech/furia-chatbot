a29241a0a03318bfb5008783161a7c2ea993af3f
Lucas Rech <lucasrech00@gmail.com>
2025-05-02 20:34:47 -0300

    feat: consumindo enpoint de atalhos

--- furia-frontend/src/services/apiService.ts
+++ furia-frontend/src/services/apiService.ts
@@ -10,6 +10,12 @@ export interface ChatResponse {
     response: string;
 }
 
+export interface Shortcut {
+    shortcut: string,
+    description: string,
+}
+
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 export async function sendMessage(data: ChatRequest): Promise<ChatResponse> {
@@ -31,4 +37,52 @@ export async function sendMessage(data: ChatRequest): Promise<ChatResponse> {
         console.error("Error sending message:", error);
         throw new Error("Failed to send message");
     }    
+}
+
+export async function getShortcuts(): Promise<Shortcut[]> {
+    try {
+        const response = await fetch(`${API_URL}shortcuts`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (!response.ok) {
+            const errorData: ErrorResponse = await response.json();
+            throw new Error(errorData.error || "Unknown error");
+        }
+
+        const result = await response.json();
+        console.log("getShortcuts: ", result);
+
+        // Verifica se o resultado é um array e contém os campos esperados
+        if (Array.isArray(result)) {
+            const shortcuts: Shortcut[] = result.map((item) => ({
+                shortcut: item.shortcut,
+                description: item.description,
+            }));
+            return shortcuts;
+        } else {
+            throw new Error("Invalid response format");
+        }
+    } catch (error) {
+        console.error("Error fetching shortcuts:", error);
+        throw new Error("Failed to fetch shortcuts");
+    }
+}
+
+export async function getCommands(): Promise<Map<string, string>> {
+    try {
+        const shortcuts = await getShortcuts(); 
+        const commands = new Map<string, string>();
+
+        // Adiciona o atalho e a descrição ao Map
+        for (const shortcut of shortcuts) {
+            commands.set(shortcut.description, shortcut.shortcut)
+        }
+
+        return commands;
+    } catch (error) {
+        console.error("Error fetching commands:", error);
+        throw new Error("Failed to fetch commands");
+    }
 }
