8832ccc61153b901b513907faa2ee338a863ec3d
Lucas Rech <lucasrech00@gmail.com>
2025-05-02 15:17:56 -0300

    feat: adicionado condição para inserir imagem do bot no chat

--- furia-frontend/src/components/Message.tsx
+++ furia-frontend/src/components/Message.tsx
@@ -1,14 +1,28 @@
 import { MessageType } from "./Chat";
 
+import pantoImg  from "../assets/panto.png";
+
 type Props = {
   message: MessageType;
 };
 
 export default function Message({ message }: Props) {
   const isUser = message.sender === "user";
+  const botImage = pantoImg;
 
   return (
     <div className={`flex ${isUser ? "justify-end" : "justify-start"}`}>
+
+      {/* Conditional rendering of the bot image */}
+      {!isUser && (
+        <img
+          src={botImage}
+          alt="Bot"
+          className="w-8 h-8 rounded-full mr-2"
+        />
+      )}
+
+      {/* Condição para diferenciar mensagem do bot e do usuário */}
       <div
         className={`px-4 py-2 rounded-xl max-w-xs ${
           isUser ? "bg-[#FF9900] text-white" : "bg-[#272727] text-white"
